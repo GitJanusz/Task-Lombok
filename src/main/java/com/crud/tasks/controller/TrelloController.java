26ccaef6886203f671984f17d9faf4416c58b26c
Janusz Pasternak <janusz_pasternak@tlen.pl>
2021-06-14 20:04:34 +0200

    Update

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -8,11 +8,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/v1/trello")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class TrelloController {
 
     private final TrelloClient trelloClient;
